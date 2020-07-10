59a0cf7f4cdef0ef1b3e903c69a1349ca6831ba7
serhan.guney <serhangny@gmail.com>
2020-07-10 15:57:05 +0300

    #serhan changes mongo deployment to manifests instead of helm deployment

--- mongodb/provision.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh -e
-
-MONGO_USER_NAME=${MONGO_USER_NAME} #user
-MONGO_PASSWD=${MONGO_PASSWD} #mongopass
-
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/ && \
-helm repo update && \
-helm install mongodb stable/mongodb \
-    --set mongodbUsername=${MONGO_USER_NAME},mongodbPassword=${MONGO_PASSWD},mongodbDatabase=weather
